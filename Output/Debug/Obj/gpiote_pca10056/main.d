4ff9710bfc504e981ef86c31124ee4f30e6ee45a
mandyliao66 <mandyliao66@gmail.com>
2025-11-21 12:46:44 -1000

    cap working 1121

--- Output/Debug/Obj/gpiote_pca10056/main.d
+++ Output/Debug/Obj/gpiote_pca10056/main.d
@@ -46,11 +46,11 @@ Output/Debug/Obj/gpiote_pca10056/main.o: \
  ../../../../../../components/libraries/util/sdk_errors.h \
  ../../../../../../components/libraries/util/sdk_resources.h \
  ../../../../../../modules/nrfx/drivers/nrfx_errors.h \
+ C:/Users/mandy/Desktop/nRF5_SDK_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/components/libraries/delay/nrf_delay.h \
+ ../../../../../../integration/nrfx/legacy/nrf_drv_saadc.h \
  ../../../../../../modules/nrfx/drivers/include/nrfx_saadc.h \
  ../../../../../../modules/nrfx/hal/nrf_saadc.h \
  C:/Users/mandy/Desktop/nRF5_SDK_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/examples/peripheral/gpiote/pca10056/blank/ses/RTT/SEGGER_RTT.h \
  C:/Users/mandy/Desktop/nRF5_SDK_17.1.0_ddde560/nRF5_SDK_17.1.0_ddde560/examples/peripheral/gpiote/pca10056/blank/ses/Config/SEGGER_RTT_Conf.h \
  C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdlib.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdarg.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/math.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/__SEGGER_RTL_FP.h
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.24/include/stdarg.h
